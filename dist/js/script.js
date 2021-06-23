9922992f70369f6caf771b877ed224bb630be1e1
Alex <alexrkoch@gmail.com>
2021-06-23 17:14:03 -0400

    refactor populateCard() function. refactor next and previous button api requests.

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,9 +5,23 @@ let nextMovieButton = document.querySelector("#navigation__next");
 let previousMovieButton = document.querySelector("#navigation__previous");
 let pageSearchResults = 1;
 let movieIndexPerPage = 0;
+let searchResults;
 
 // thinking about "clean code" I think we want to break this up into
 // multiple functions if possible. "Each function should do just 1 thing"
+
+const populateCard = (object) => {
+  document.getElementById("current-movie-title").innerHTML =
+    object.data.results[movieIndexPerPage].original_title;
+  document.getElementById("details__rating--number").innerHTML =
+    object.data.results[movieIndexPerPage].vote_average;
+  document.getElementById("details__summary--body").innerHTML =
+    object.data.results[movieIndexPerPage].overview;
+  document.getElementById(
+    "details__img"
+  ).src = `https://image.tmdb.org/t/p/w500${object.data.results[movieIndexPerPage].poster_path}`;
+};
+
 const searchMovie = (event) => {
   const url = `http://localhost:5001/api/discover/${movieYear.value}/${genreDropdown.value}/${pageSearchResults}`;
   fetch(url)
@@ -21,31 +35,20 @@ const searchMovie = (event) => {
       (networkError) => console.log(networkError.message)
     )
     .then((jsonResponse) => {
-      document.getElementById("current-movie-title").innerHTML =
-        jsonResponse.data.results[movieIndexPerPage].original_title;
-      document.getElementById("details__rating--number").innerHTML =
-        jsonResponse.data.results[movieIndexPerPage].vote_average;
-      document.getElementById("details__summary--body").innerHTML =
-        jsonResponse.data.results[movieIndexPerPage].overview;
-      document.getElementById(
-        "details__img"
-      ).src = `https://image.tmdb.org/t/p/w500${jsonResponse.data.results[movieIndexPerPage].poster_path}`;
+      searchResults = jsonResponse;
+      populateCard(jsonResponse);
     });
 };
 
 searchMovieButton.addEventListener("click", searchMovie);
 
-// the current functionality below for nextMovie() and previousMovie()
-// is to call searchMovie() again, but I'm realizing this is probably
-// a non-optimized solution, as it re-sends our fetch request every time
-// you go back or forth. Our requests are limited right? We could instead
-// save the full request results into a JS object locally, then query that
-// object for our next and previous buttons.
 const nextMovie = () => {
   if (movieIndexPerPage < 19) {
     movieIndexPerPage += 1;
-  }
-  searchMovie();
+  } // else {pageSearchResults += 1;
+  // searchMovie()
+  // }
+  populateCard(searchResults);
 };
 
 nextMovieButton.addEventListener("click", nextMovie);
@@ -54,7 +57,7 @@ const previousMovie = () => {
   if (movieIndexPerPage > 0) {
     movieIndexPerPage -= 1;
   }
-  searchMovie();
+  populateCard(searchResults);
 };
 
 previousMovieButton.addEventListener("click", previousMovie);
