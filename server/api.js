5bddfdc397403e0a9da939299301b00f5b86522a
Austin <austin.kraft@gmail.com>
2021-06-22 21:19:08 -0700

    added configuration route

--- server/api.js
+++ server/api.js
@@ -4,9 +4,11 @@ const apiRouter = express.Router();
 const moviesRouter = require('./movies');
 const genresRouter = require('./genres');
 const discoverRouter = require('./discover');
+const configurationRouter = require('./configuration');
 
 apiRouter.use('/movies', moviesRouter);
 apiRouter.use('/genres', genresRouter);
 apiRouter.use('/discover', discoverRouter);
+apiRouter.use('/configuration', configurationRouter);
 
 module.exports = apiRouter;
---
+++ server/configuration.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const configurationRouter = express.Router();
+const MovieDB = require('node-themoviedb');
+const mdb = new MovieDB('ad65f3f346cc7869630deac544b0f1f7');
+
+module.exports = configurationRouter;
+
+configurationRouter.get('/', async (req, res, next) => {
+    try {
+        const args = {
+            pathParameters: {
+
+            },
+            query: {
+                // query string, i.e. session_id
+                // NOTE: api_key and language will be added to query by default, don't need specify these values
+            },
+        };
+        const movie = await mdb.configuration.getAPIConfiguration(args);
+        res.status(200).send(movie);
+        /*
+        {
+            data: Object. Parsed json data of response
+            headers: Object. Headers of response
+        }
+        */
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
+});
+
+
+
+// `http://localhost:5001/api/config/${movieYear.value}/${genreDropdown.value}/${pageSearchResults}`
+
+
+// let APIKEY = 'ad65f3f346cc7869630deac544b0f1f7';
+// let baseURL = 'https://api.themoviedb.org/3/';
+// let configData = null;
+// let baseImageURL = null;
+
+// let getConfig = function () {
+//     let url = "".concat(baseURL, 'configuration?api_key=', APIKEY);
+//     fetch(url)
+//     .then((result)) => {
+//         return result.json();
+//     })
+//     .then((data) => {
+//         baseImageURL = data.images.secure_base_url;
+//         configData = data.images;
+//         console.log('config:', data);
+//         console.log('config fetched');
+//         //runSearch?
+//     })
+// };
