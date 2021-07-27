b984b47a722d9018c9b74066e195b47860f20f74
Alex <alexrkoch@gmail.com>
2021-07-27 12:38:53 -0400

    change port to 5000, add node version

--- express-server.js
+++ express-server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000;
 
-const apiRouter = require('./server/api');
-app.use('/api', apiRouter);
+const apiRouter = require("./server/api");
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
+  console.log(`Server listening on port ${PORT}`);
+});
 
 module.exports = app;
