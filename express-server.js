371aebe7273a868aca3b120259c6b4d435ae42b6
Alex <alexrkoch@gmail.com>
2021-07-29 11:58:48 -0400

    add an express.static directory to express-server.js

--- express-server.js
+++ express-server.js
@@ -7,6 +7,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname));
 const PORT = process.env.PORT || 5001;
 
 const apiRouter = require("./server/api");
